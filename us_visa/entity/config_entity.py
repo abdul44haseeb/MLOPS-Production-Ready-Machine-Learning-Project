5fbcfadc72b60320df1e4f067e58ea56bf178dc1
Abdul Haseeb A <abdul44haseeb@gmail.com>
2024-09-11 10:45:41 +0530

    data validation added

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -3,16 +3,18 @@ from us_visa.constants import *
 from dataclasses import dataclass
 from datetime import datetime
 
-TIMESTAMP:str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
 
 @dataclass
 class TrainingPipelineConfig:
-    pipeline_name:str = PIPELINE_NAME
-    artifact_dir:str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
-    timestamp:str = TIMESTAMP
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
 
-training_pipeline_config : TrainingPipelineConfig = TrainingPipelineConfig()
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
 
 
 @dataclass
@@ -23,3 +25,17 @@ class DataIngestionConfig:
     testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
     train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
     collection_name:str = DATA_INGESTION_COLLECTION_NAME
+
+
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
+                                               DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+    
+
+
+
+
+
